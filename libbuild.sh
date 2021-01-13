482ad68a1c2c8c9028b07602a468b2e87f42a763
mihalin <stopa3.10.1996@mail.ru>
2021-01-13 13:26:48 +0300

    Improve README, add linux build scripts, win32, win64

--- libbuild.sh
+++
@@ -1,2 +0,0 @@
-g++ lib.cpp `pkg-config --cflags aravis-0.8`
-g++ -shared -o libara.so lib.o `pkg-config --libs aravis-0.8`
---
+++ libbuild64.bat
@@ -0,0 +1,3 @@
+del libara.dll
+C:\msys64\mingw64\bin\g++ -c lib.cpp -IC:\msys64\mingw64\include -IC:\msys64\mingw64\lib\glib-2.0\include -IC:\msys64\mingw64\include\glib-2.0 -IC:\msys64\mingw64\include\aravis-0.8
+C:\msys64\mingw64\bin\g++ -shared -o libara.dll lib.o -laravis-0.8 -LC:\msys64\mingw64\lib -lxml2 -lglib-2.0 -lm -pthread -lgio-2.0 -lgobject-2.0 -lgthread-2.0 -lz
